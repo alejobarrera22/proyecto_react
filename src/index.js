bbcc834f82476d397d4a5a9d05c6ef467ac5bf28
Alejo Exequiel Barrera <alejo.barrera02@gmail.com>
2023-06-25 23:42:41 -0300

    m

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { StrictMode } from 'react';
+//import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
 
@@ -12,6 +12,7 @@ const firebaseConfig = {
     projectId: "tiendamoto-c9051",
     storageBucket: "tiendamoto-c9051.appspot.com",
     messagingSenderId: "298231577511",
+
     appId: "1:298231577511:web:0faf568d8c733097391482",
     measurementId: "G-YK9JXTECQY"
 };
@@ -26,8 +27,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //     </StrictMode>
 //     rootElement
 // );
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 root.render(<App />);
 
 
